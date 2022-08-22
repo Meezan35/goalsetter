6eacbe294a764baecc2c87185a7d06a6bab155d0
Meezan.Shaikh <Meezan.Shaikh@ril.com>
2022-08-22 19:58:25 +0530

    added frontend ui

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const connectDB = require('./config/db')
 
 
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 connectDB()
 const app = express();
 
@@ -16,6 +16,7 @@ app.use(express.urlencoded({extended:false}));
 
 
 app.use('/api/goals',require('./routes/goalRoutes'))
+app.use('/api/users',require('./routes/userRoutes'))
 app.use(errorHandler);
 
 app.listen(port,()=>console.log(`Server started on port ${port}`))
